504fba7654ad6d5e88a2d1932100ee1501bc806e
kjdiaz508 <diazkevin508@gmail.com>
2024-10-21 17:23:48 -0700

    set up basic structure and page routing

---
+++ packages/react-frontend/src/main.jsx
@@ -0,0 +1,23 @@
+// src/main.jsx
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./home/Home";
+import Calendar from "./calendar/Calendar";
+import "./styles/global.css";
+
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render:
+root.render(
+  <Router>
+    <Routes>
+      {/* Define routes here */}
+      <Route path="/" element={<Home />} />
+      <Route path="/calendar" element={<Calendar />} />
+    </Routes>
+  </Router>
+);
