eda3066aa858e1730c71de8f222ce0b456ded09c
kjdiaz508 <diazkevin508@gmail.com>
2024-10-25 14:39:31 -0700

    background component

---
+++ packages/react-frontend/src/components/Background.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/background.css";
+
+function Background() {
+  return (
+    <div>
+      <div>
+        <div>
+          <div class="grid-lines-container">
+            <div class="grid-lines"></div>
+          </div>
+          <div class="vignette"></div>
+        </div>
+        <div class="centering-is-hard"></div>
+      </div>
+    </div>
+  );
+}
+export default Background;
