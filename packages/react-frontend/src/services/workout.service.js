7a5c1403d82970e7807b9c81a34bfb9f87f1ccef
Sai <saimurthy66@gmail.com>
2024-11-05 22:43:10 -0800

    Initial changes adding authentication to backend and setting up tests to test the backend. Also creating services for the frontend so that it is easier to do some operations in the future

---
+++ packages/react-frontend/src/services/workout.service.js
@@ -0,0 +1,21 @@
+export const WorkoutService = {
+    async getWorkouts() {
+        return ApiService.get('/api/workouts');
+    },
+
+    async createWorkout(workoutData) {
+        return ApiService.post('/api/workouts/strength', workoutData);
+    },
+
+    async getWorkoutById(id) {
+        return ApiService.get(`/api/workouts/${id}`);
+    },
+
+    async updateWorkout(id, workoutData) {
+        return ApiService.put(`/api/workouts/${id}`, workoutData);
+    },
+
+    async deleteWorkout(id) {
+        return ApiService.delete(`/api/workouts/${id}`);
+    }
+};
