6909114856d56b5e6743275175911ef52d554847
Ida Voong <idavoong@gmail.com>
2024-11-08 21:33:55 -0800

    import basic calendar

--- packages/react-frontend/src/calendar/Calendar.jsx
+++ packages/react-frontend/src/calendar/Calendar.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Calendar from "react-calendar";
 
-function Calendar() {
+import 'react-calendar/dist/Calendar.css';
+
+// type ValuePiece = Date | null;
+
+// type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+function Calendar1() {
+  const [value, onChange] = useState(new Date());
+
   return (
     <div>
       <h1>Hello, Calendar!</h1>
       <p>Click the link to view Home:</p>
       <Link to="/">Go to Home</Link>
+      <Calendar onChange={onChange} value={value} />
     </div>
   );
 }
-export default Calendar;
+export default Calendar1;
