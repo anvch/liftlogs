c57188c45078d2fa2c6761b387facf2f4f7874c4
Angela Chen <achen313@calpoly.edu>
2024-11-22 13:30:24 -0800

    change port to 8080

--- packages/workout-backend/src/server.js
+++ packages/workout-backend/src/server.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
 
 // Only start server if not in test mode
 if (process.env.NODE_ENV !== "test") {
-  const PORT = process.env.PORT || 3001;
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
