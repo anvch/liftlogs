dc3f9b6733bd8f65ceb45631101a9bab35dbd974
Angela Chen <achen313@calpoly.edu>
2024-11-28 01:37:58 -0800

    user context for username on home

--- packages/react-frontend/src/home/Home.jsx
+++ packages/react-frontend/src/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"; // eslint-disable-line no-unused-vars
+import React, { useState, useEffect, useContext } from "react"; // eslint-disable-line no-unused-vars
 import { Link } from "react-router-dom";
 import Background from "../components/Background";
 import WorkoutCalendar from "../components/WorkoutCalendar";
@@ -6,23 +6,16 @@ import MyWorkouts from "../components/MyWorkouts";
 import styles from "./home.module.css";
 import profileIcon from "../assets/profile.svg";
 import { WorkoutService } from "../services/workout.service";
+import { UserContext } from "../contexts/UserContext";
 
 function Home() {
-  const [username, setUsername] = useState("");
+  const { user, loading } = useContext(UserContext); // Use context to get the user and loading state
   const [presets, setPresets] = useState([]);
 
   useEffect(() => {
     const fetchPresets = async () => {
       try {
         const data = await WorkoutService.getPresets();
-
-        // Assuming 'data' is an array of workouts and each workout contains a 'username' field
-        // therefore, can only get username if they have one workout?
-        // TODO: debug this
-        if (data.length > 0) {
-          const firstWorkout = data[0]; // use the first workout's username
-          setUsername(firstWorkout.username || "Unknown User");
-        }
         setPresets(data);
       } catch (error) {
         console.error("Error fetching presets:", error);
@@ -43,11 +36,13 @@ function Home() {
       </div>
       <br></br>
       <br></br>
-      <h1 className={styles.title}>Welcome, {username}!</h1>
+      <h1 className={styles.title}>
+        {loading ? "Loading..." : `Welcome, ${user.username}!`}
+      </h1>
       <WorkoutCalendar />
       <br></br>
       <div className={styles.navbar}>
-        <Link to="/addworkout">
+        <Link to="/workout-entry">
           <button> + Add Workout</button>
         </Link>
       </div>
